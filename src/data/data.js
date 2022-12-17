6c8a62e447aeebb81fb502c42602cec14170c5ba
zanehirning <a02371568@usu.edu>
2022-12-17 01:19:49 -0700

    new-project-WIP

--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,7 @@
 import Snake from '../assets/projects/Snake.gif'
 import WebCrawler from '../assets/projects/web_crawler.jpg'
 import FlappyBird from '../assets/projects/flappy-bird.jpg'
+import TwentyFortyEight from '../assets/projects/2048.jpg'
 
 export const data=[
     {
@@ -21,6 +22,13 @@ export const data=[
     {
         id:3,
         name:"FlappyBird Game AI",
+        image:FlappyBird,
+        github:"https://github.com/zanehirning/Flappy-Bird-AI",
+        live:"",
+    },
+    {
+        id:4,
+        name:"2048 Game AI (W.I.P)",
         image:FlappyBird,
         github:"https://github.com/zanehirning/Flappy-Bird-AI",
         live:"",
